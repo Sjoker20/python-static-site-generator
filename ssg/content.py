79f4016b037bc313c58204b7f45c8e35a051465a
Shanu <96528642+Sjoker20@users.noreply.github.com>
2023-01-03 23:57:35 +0530

    Completed module3

--- ssg/content.py
+++ ssg/content.py
@@ -4,11 +4,11 @@ from yaml import load,FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter= r"^(?:-|\+){3}\s*$"
+    __delimiter= r"^(?:-|\+){3}\s*$"
     __regex= re.compile(__delimiter, constant=re.MULTLINE)
     
     
-    @Classmethod
+    @classmethod
     def load(cls,string):
         _,fm,content= cls.__regex.split(string,2)
         metadata = load(fm,Loader=FullLoader)
